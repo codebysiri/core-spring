73fb3abc399789aace1eb4c59321a4de2a0f227f
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-04-24 13:42:57 +0900

    Update project

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -9,11 +9,11 @@ public class MemberApp {
 
     public static void main(String[] args) {
         MemberService memberService = new MemberServiceImpl();
-        Member member = new Member(1L, "MemberA", Grade.VIP);
+        Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println("member = " + member.getName());
+        System.out.println("new member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
     }
 }
